e98fd2febe2942a5c0b42df18753a3583f32f816
Alexndr <mugz_play@rambler.ru>
2025-01-15 14:11:56 +0400

    update grammatic

--- grammer.py
+++ grammer.py
@@ -1,4 +1,6 @@
 import os
+from scanner import Scanner
+from scanner import SymbolTableManager
 
 # Класс для представления токенов
 class Token:
@@ -14,10 +16,15 @@ class Parser:
     def __init__(self, scanner):
         self.scanner = scanner
         self.current_token = None
+        self.flag = False
         self.advance()
 
     def advance(self):
-        self.current_token = self.scanner.get_next_token()
+        token = self.scanner.get_next_token()
+        type = token[0]
+        value = token[1]
+        print(f"Current token: {type} {value}")
+        self.current_token = Token(type, value)
 
     def parse(self):
         try:
@@ -28,7 +35,7 @@ class Parser:
 
     def program(self):
         statements = []
-        while self.current_token:
+        while self.current_token and self.flag is False:
             statements.append(self.statement())
         return statements
 
@@ -39,6 +46,17 @@ class Parser:
             return self.assignment()
         elif self.current_token.type == 'KEYWORD' and self.current_token.value == 'write':
             return self.write_statement()
+        elif self.current_token.type == 'KEYWORD' and self.current_token.value == 'read':
+            return self.read_statement()
+        elif self.current_token.type == 'KEYWORD' and self.current_token.value == 'if':
+            return self.if_statement()
+        elif self.current_token.type == 'KEYWORD' and self.current_token.value == 'while':
+            return self.while_statement()
+        elif self.current_token.type == 'KEYWORD' and self.current_token.value == 'for':
+            return self.for_statement()
+        elif self.current_token.type == 'KEYWORD' and self.current_token.value == 'end':
+            print("End of program")
+            self.flag = True
         else:
             raise SyntaxError(f"Unexpected token: {self.current_token}")
 
@@ -67,7 +85,44 @@ class Parser:
             expressions.append(self.expression())
         self.match('RAZD', ')')
         return ('write', expressions)
+    
+    def read_statement(self):
+        self.match('KEYWORD', 'read')
+        self.match('RAZD', '(')
+        identifiers = [self.match('ID')]
+        while self.current_token and self.current_token.type == 'RAZD' and self.current_token.value == ',':
+            self.match('RAZD', ',')
+            identifiers.append(self.match('ID'))
+        self.match('RAZD', ')')
+        return ('read', identifiers)
 
+    def if_statement(self):
+        self.match('KEYWORD', 'if')
+        condition = self.expression()
+        self.match('KEYWORD', 'then')
+        then_branch = self.statement()
+        else_branch = None
+        if self.current_token and self.current_token.type == 'KEYWORD' and self.current_token.value == 'else':
+            self.match('KEYWORD', 'else')
+            else_branch = self.statement()
+        return ('if', condition, then_branch, else_branch)
+
+    def while_statement(self):
+        self.match('KEYWORD', 'while')
+        condition = self.expression()
+        self.match('KEYWORD', 'do')
+        body = self.statement()
+        return ('while', condition, body)
+
+    def for_statement(self):
+        self.match('KEYWORD', 'for')
+        initialization = self.assignment()
+        self.match('KEYWORD', 'to')
+        condition = self.expression()
+        self.match('KEYWORD', 'do')
+        body = self.statement()
+        return ('for', initialization, condition, body)
+    
     def expression(self):
         operand = self.operand()
         while self.current_token and self.current_token.type == 'RAZD' and self.current_token.value in ['NE', 'EQ', 'LT', 'LE', 'GT', 'GE']:
@@ -112,6 +167,8 @@ class Parser:
             expr = self.expression()
             self.match('RAZD', ')')
             return expr
+        elif self.current_token.type == 'EOF':
+            print("EOF reached")
         else:
             raise SyntaxError(f"Unexpected token: {token}")
 
@@ -124,70 +181,68 @@ class Parser:
             raise SyntaxError(f"Expected {type} {value}, but got {self.current_token}")
 
 # Семантический анализатор
-class SemanticAnalyzer:
-    def __init__(self, ast):
-        self.ast = ast
-        self.symbol_table = {}
+#class SemanticAnalyzer:
+   # def __init__(self, ast):
+    #    self.ast = ast
+     #   self.symbol_table = {}
+#
+ #   def analyze(self):
+  #      try:
+   #         for statement in self.ast:
+    #            self.visit(statement)
+     #   except SemanticError as e:
+      #      print(f"Semantic Error: {e}")
+#
+ #   def visit(self, node):
+  #      method_name = 'visit_' + node[0]
+   #     visitor = getattr(self, method_name, self.generic_visit)
+    #    return visitor(node)
+#
+ #   def visit_declaration(self, node):
+  #      _, identifiers, type_ = node
+   #     for identifier in identifiers:
+    #        if identifier.value in self.symbol_table:
+     #           raise SemanticError(f"Identifier {identifier.value} already declared")
+      #      self.symbol_table[identifier.value] = type_.value
 
-    def analyze(self):
-        try:
-            for statement in self.ast:
-                self.visit(statement)
-        except SemanticError as e:
-            print(f"Semantic Error: {e}")
+    #def visit_assignment(self, node):
+     #   _, identifier, expression = node
+      #  if identifier.value not in self.symbol_table:
+       #     raise SemanticError(f"Identifier {identifier.value} not declared")
+        #self.visit(expression)
 
-    def visit(self, node):
-        method_name = 'visit_' + node[0]
-        visitor = getattr(self, method_name, self.generic_visit)
-        return visitor(node)
+    #def visit_write(self, node):
+     #   _, expressions = node
+      #  for expression in expressions:
+       #     self.visit(expression)
 
-    def visit_declaration(self, node):
-        _, identifiers, type_ = node
-        for identifier in identifiers:
-            if identifier.value in self.symbol_table:
-                raise SemanticError(f"Identifier {identifier.value} already declared")
-            self.symbol_table[identifier.value] = type_.value
+#    def visit_binary_op(self, node):
+ #       _, operator, left, right = node
+  #      self.visit(left)
+   #     self.visit(right)
 
-    def visit_assignment(self, node):
-        _, identifier, expression = node
-        if identifier.value not in self.symbol_table:
-            raise SemanticError(f"Identifier {identifier.value} not declared")
-        self.visit(expression)
+    #def visit_unary_op(self, node):
+     #   _, operator, operand = node
+      #  self.visit(operand)
 
-    def visit_write(self, node):
-        _, expressions = node
-        for expression in expressions:
-            self.visit(expression)
+    #def visit_identifier(self, node):
+     #   _, value = node
+      #  if value not in self.symbol_table:
+       #     raise SemanticError(f"Identifier {value} not declared")
 
-    def visit_binary_op(self, node):
-        _, operator, left, right = node
-        self.visit(left)
-        self.visit(right)
+    #def visit_number(self, node):
+     #   pass
 
-    def visit_unary_op(self, node):
-        _, operator, operand = node
-        self.visit(operand)
+    #def visit_boolean(self, node):
+     #   pass
 
-    def visit_identifier(self, node):
-        _, value = node
-        if value not in self.symbol_table:
-            raise SemanticError(f"Identifier {value} not declared")
-
-    def visit_number(self, node):
-        pass
-
-    def visit_boolean(self, node):
-        pass
-
-    def generic_visit(self, node):
-        raise SemanticError(f"No visit_{node[0]} method")
+    #def generic_visit(self, node):
+     #   raise SemanticError(f"No visit_{node[0]} method")
 
 # Основная функция для выполнения анализа
 def main():
     input_file_path = os.path.join(os.path.dirname(__file__), 'main.txt')
-    
-    from scanner import Scanner
-
+    SymbolTableManager.init()
     scanner = Scanner(input_file_path)
     parser = Parser(scanner)
     ast = parser.parse()
@@ -195,8 +250,8 @@ def main():
         print("Abstract Syntax Tree (AST):")
         print(ast)
 
-        semantic_analyzer = SemanticAnalyzer(ast)
-        semantic_analyzer.analyze()
+        #semantic_analyzer = SemanticAnalyzer(ast)
+       # semantic_analyzer.analyze()
         print("Semantic analysis completed successfully.")
     else:
         print("Parsing failed.")
